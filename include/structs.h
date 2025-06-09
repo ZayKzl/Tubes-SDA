82cb5c67025156c5988ef08de36de45f37229ec3
zahraldila <zaraliskandar@gmail.com>
2025-06-09 20:13:49 +0700

    perbaikan avl dinamis

--- include/structs.h
+++ include/structs.h
@@ -13,14 +13,14 @@ typedef struct {
     char id_paper[MAX_ID_PAPER];
     char field_of_study[MAX_FIELD_STUDY];
     char judul[MAX_JUDUL];
-    int tahun_terbit;
-    char sitasi_info[MAX_SITASI_INFO];
+    int tahun_terbit;       
+    char sitasi_info[MAX_SITASI_INFO]; 
     char nama_penulis[MAX_PENULIS];
     char abstrak[MAX_ABSTRAK];
-    int jumlah_incitations;
+    int jumlah_incitations; 
 } JurnalData;
 
-typedef struct PaperNode {
+typedef struct PaperNode { 
     JurnalData data;
     struct PaperNode* next;
     struct PaperNode* prev;
@@ -29,17 +29,20 @@ typedef struct PaperNode {
 typedef struct BSTNodeField {
     char field_of_study[MAX_FIELD_STUDY];
 
-    PaperNode* year_list_head;          // Urut Tahun MENAIK (Terlama -> Terbaru)
+    PaperNode* year_list_head;         // Urut Tahun MENAIK (Terlama -> Terbaru)
     PaperNode* year_list_tail;
 
-    PaperNode* incitations_list_head;   // Urut Incitations MENURUN (Terbanyak -> Tersedikit)
+    PaperNode* incitations_list_head;  // Urut Incitations MENURUN (Terbanyak -> Tersedikit)
     PaperNode* incitations_list_tail;
 
-    PaperNode* title_list_head;         // Urut Judul MENAIK (A -> Z)
+    PaperNode* title_list_head;        // Urut Judul MENAIK (A -> Z)
     PaperNode* title_list_tail;
 
     struct BSTNodeField* left_child;
     struct BSTNodeField* right_child;
+    
+    // PENAMBAHAN UTAMA UNTUK AVL DINAMIS
+    int height; // Tinggi subtree yang berakar pada node ini
 } BSTNodeField;
 
 #endif // STRUCTS_H
